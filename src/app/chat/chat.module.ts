b3ff1b178fc1434f956bba844a24781e4c48b1e1
João Paulo Oliveira dos Santos <jposrj@gmail.com>
2018-07-26 21:59:34 -0300

    criação do modulo e componente chat

---
+++ src/app/chat/chat.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChatDialogComponent } from './chat-dialog/chat-dialog.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ChatDialogComponent]
+})
+export class ChatModule { }
